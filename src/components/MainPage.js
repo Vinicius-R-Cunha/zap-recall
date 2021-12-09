d1e2711991a5a2fc7c8d9f6cb56bd493d5b14421
Vinicius <vinicunha2008@gmail.com>
2021-12-09 20:39:10 -0300

    bonus 1,2,3

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,8 +1,18 @@
-export default function MainPage({ page, setPage, ending }) {
+export default function MainPage({ page, setPage, ending, zapGoal, setZapGoal, questionsQuantity }) {
+    function goToFlashcards() {
+        if (zapGoal >= 1 && zapGoal % 1 === 0 && zapGoal <= questionsQuantity) {
+            setPage('hidden');
+        } else {
+            alert(`Digite um número inteiro entre 0 e ${questionsQuantity}`);
+            setZapGoal('');
+        }
+    }
+
     return (
         <div className={`main-page ${page} ${ending === 'hidden' ? '' : 'hidden'}`}>
             <img className="logo" src="assets/logo.png" alt="" />
-            <div className="start-button" data-identifier="start-zap-recall" onClick={() => setPage('hidden')}>
+            <input className="zap-goal" placeholder="Sua meta de zaps" type="text" value={zapGoal} onInput={e => setZapGoal(e.target.value)} />
+            <div className="start-button" data-identifier="start-zap-recall" onClick={goToFlashcards}>
                 Praticar React
                 <img src="assets/next.png" alt="" />
             </div>
