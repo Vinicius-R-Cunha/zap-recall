6fee2c617272cfa6e44704db57be1c5a3b5a234e
Vinicius <vinicunha2008@gmail.com>
2021-12-13 14:43:44 -0300

    styles adjust and array of questions set

--- src/components/Flashcards.js
+++ src/components/Flashcards.js
@@ -1,10 +1,12 @@
 import Flashcard from "./Flashcard";
 import LogoMini from "./LogoMini";
+import QuizzTitle from "./QuizzTitle";
 
-export default function Flashcards({ page, ending, setEnding, setSuccessPage, zapGoal, quizz, incorrectAnswers, setIncorrectAnswers }) {
+export default function Flashcards({ page, ending, setEnding, setSuccessPage, zapGoal, quizz, quizzTitle, incorrectAnswers, setIncorrectAnswers }) {
     return (
         <div className={`flashcards ${page === 'show' ? 'hidden' : 'show'} ${ending === 'hidden' ? '' : 'hidden'}`}>
             <LogoMini />
+            <QuizzTitle quizzTitle={quizzTitle} />
             <Flashcard setEnding={setEnding} setSuccessPage={setSuccessPage} zapGoal={zapGoal} quizz={quizz} incorrectAnswers={incorrectAnswers} setIncorrectAnswers={setIncorrectAnswers} />
         </div>
     );
---
+++ src/components/QuizzTitle.js
@@ -0,0 +1,5 @@
+export default function QuizzTitle({ quizzTitle }) {
+    return (
+        <p className="quizz-title">{quizzTitle}</p>
+    );
+}
