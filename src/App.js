ac5559186a841285756e568834679a35cd3e1550
Vinicius <vinicunha2008@gmail.com>
2021-12-13 14:43:44 -0300

    versão final 1

--- src/App.js
+++ src/App.js
@@ -4,34 +4,19 @@ import MainPage from "./components/MainPage";
 import EndingScreen from "./components/EndingScreen";
 
 export default function App() {
-    // const quizz = [
-    //     { question: "O que é JSX ?", answer: "Uma extensão de linguagem do JavaScript" },
-    //     { question: "O React é __", answer: "uma biblioteca JavaScript para construção de interfaces" },
-    //     { question: "Componentes devem iniciar com __", answer: "letra maiúscula" },
-    //     { question: "Podemos colocar __ dentro do JSX", answer: "expressões" },
-    //     { question: "O ReactDOM nos ajuda __", answer: "interagindo com a DOM para colocar componentes React na mesma" },
-    //     { question: "Usamos o npm para __", answer: "gerenciar os pacotes necessários e suas dependências" },
-    //     { question: "Usamos props para __", answer: "passar diferentes informações para componentes" },
-    //     { question: "Usamos estado(state) para __", answer: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente" }
-    // ];
 
-    const quizz = [
-        { question: "O que é JSX ?", answer: "Uma extensão de linguagem do JavaScript" },
-        { question: "O React é __", answer: "uma biblioteca JavaScript para construção de interfaces" }
-    ]
-
+    let [quizz, setQuizz] = useState([{ question: "", answer: "" }]);
+    let [quizzTitle, setQuizzTitle] = useState('');
     let [incorrectAnswers, setIncorrectAnswers] = useState(0);
+    let [zapGoal, setZapGoal] = useState('');
     let [page, setPage] = useState('show');
     let [ending, setEnding] = useState('hidden');
     let [successPage, setSuccessPage] = useState('hidden');
-    let [zapGoal, setZapGoal] = useState('');
-
-    const questionsQuantity = quizz.length;
 
     return (
         <>
-            <MainPage page={page} setPage={setPage} ending={ending} zapGoal={zapGoal} setZapGoal={setZapGoal} questionsQuantity={questionsQuantity} />
-            <Flashcards page={page} ending={ending} setEnding={setEnding} setSuccessPage={setSuccessPage} zapGoal={zapGoal} quizz={quizz} questionsQuantity={questionsQuantity} incorrectAnswers={incorrectAnswers} setIncorrectAnswers={setIncorrectAnswers} />
+            <MainPage page={page} setPage={setPage} ending={ending} zapGoal={zapGoal} setZapGoal={setZapGoal} setQuizz={setQuizz} setQuizzTitle={setQuizzTitle} />
+            <Flashcards page={page} ending={ending} setEnding={setEnding} setSuccessPage={setSuccessPage} zapGoal={zapGoal} quizz={quizz} quizzTitle={quizzTitle} incorrectAnswers={incorrectAnswers} setIncorrectAnswers={setIncorrectAnswers} />
             <EndingScreen ending={ending} setEnding={setEnding} successPage={successPage} incorrectAnswers={incorrectAnswers} />
         </>
     );
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,6 +1,39 @@
-export default function MainPage({ page, setPage, ending, zapGoal, setZapGoal, questionsQuantity }) {
+import StartButton from "./StartButton";
 
-    function goToFlashcards() {
+export default function MainPage({ setQuizz, setQuizzTitle, page, setPage, ending, zapGoal, setZapGoal }) {
+    const reactQuizz = [
+        { question: "O que é JSX ?", answer: "Uma extensão de linguagem do JavaScript" },
+        { question: "O React é __", answer: "uma biblioteca JavaScript para construção de interfaces" },
+        { question: "Componentes devem iniciar com __", answer: "letra maiúscula" },
+        { question: "Podemos colocar __ dentro do JSX", answer: "expressões" },
+        { question: "O ReactDOM nos ajuda __", answer: "interagindo com a DOM para colocar componentes React na mesma" },
+        { question: "Usamos o npm para __", answer: "gerenciar os pacotes necessários e suas dependências" },
+        { question: "Usamos props para __", answer: "passar diferentes informações para componentes" },
+        { question: "Usamos estado(state) para __", answer: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente" }
+    ];
+
+    const javascriptQuizz = [
+        { question: "Qual o papel do JavaScript na construção de um site?", answer: "Nos permite criar sites mais interativos, reagindo ao comportamento do usuário" },
+        { question: "O que é Árvore DOM", answer: "É a árvore de elementos do HTML representada como objetos no JavaScript" },
+        { question: "O que são Higher-Order Functions?", answer: "São funções que recebem outras funções como parâmetro" },
+        { question: "Qual a melhor maneira de alterar estilos CSS através do JavaScript?", answer: "Adicionando ou removendo classes conforme a necessidade" },
+        { question: "O que é um evento no JavaScript?", answer: "Ações ou ocorrências que acontecem nos elementos acarretadas pelo usuário" }
+    ];
+
+    let questionsQuantity;
+
+    function goToFlashcards(name) {
+
+        setQuizzTitle(name);
+
+        if (name === 'React') {
+            setQuizz([...reactQuizz]);
+            questionsQuantity = reactQuizz.length;
+        } else if (name === 'Javascript') {
+            setQuizz([...javascriptQuizz]);
+            questionsQuantity = javascriptQuizz.length;
+        }
+
         if (zapGoal >= 1 && zapGoal % 1 === 0 && zapGoal <= questionsQuantity) {
             setPage('hidden');
         } else {
@@ -13,10 +46,8 @@ export default function MainPage({ page, setPage, ending, zapGoal, setZapGoal, q
         <div className={`main-page ${page} ${ending === 'hidden' ? '' : 'hidden'}`}>
             <img className="logo" src="assets/logo.png" alt="" />
             <input className="zap-goal" placeholder="Sua meta de zaps" type="text" value={zapGoal} onInput={e => setZapGoal(e.target.value)} />
-            <div className="start-button" data-identifier="start-zap-recall" onClick={goToFlashcards}>
-                Praticar React
-                <img src="assets/next.png" alt="" />
-            </div>
+            <StartButton name="Javascript" goToFlashcards={goToFlashcards} />
+            <StartButton name="React" goToFlashcards={goToFlashcards} />
         </div>
     );
 }
